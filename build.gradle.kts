4d4640a69d47f89926de134d023308a13ac1e02e
Itay-Biton <itaybit10@gmail.com>
2025-01-30 10:26:03 +0200

    Publish Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,18 +4,3 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.maven.publish) // Apply the maven-publish plugin
 }
-
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                groupId = "com.github.ItayBiton" 
-                artifactId = "GamificationLib"
-                version = "1.0.0"
-                artifact(tasks.getByName("bundleReleaseAar"))
-
-                // Add dependencies to the Maven publication configuration (api or implementation) 
-            }
-        }
-    }
-}
